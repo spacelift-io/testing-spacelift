08f2c34e070ba743556ee7020b74037eb2c62f7b
Marcin Wyszynski <marcinw@spacelift.io>
2020-08-28 12:57:43 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "spacelift" {}
+
+terraform {
+  required_providers {
+    spacelift = {
+      source = "registry.spacelift.io/spacelift-io/spacelift"
+    }
+  }
+}
+
+resource "spacelift_stack" "kaboom" {
+  name           = "kaboom"
+  branch         = "master"
+  repository     = "demo"
+  worker_pool_id = "01EGJJ0HNKQW51GSJY2F8Q077Z"
+}

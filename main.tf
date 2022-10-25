ead48937bf4c728c3ad12ca1a7b96d13c024b346
Marcin Wyszynski <marcinw@spacelift.io>
2022-10-25 16:14:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "random_string" "test" {
   length = 15
 }
 
-resource "time_offset" "example" {
+resource "time_offset" "offset" {
   offset_days = 7
+}
+
+moved {
+  from = time_offset.example
+  to   = time_offset.offset
 }
